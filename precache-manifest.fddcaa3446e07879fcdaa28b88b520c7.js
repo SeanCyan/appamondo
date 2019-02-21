efd790255389d270494a47f65f5c458ef27a3e23
Sean Nattress <snattress5@googlemail.com>
2019-02-21 20:26:02 +0000

    Updates

---
+++ precache-manifest.fddcaa3446e07879fcdaa28b88b520c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "897d2113175b9b6c777c",
+    "url": "/appamondo/static/js/runtime~main.897d2113.js"
+  },
+  {
+    "revision": "3fb1a0735c8b139113f5",
+    "url": "/appamondo/static/js/main.3fb1a073.chunk.js"
+  },
+  {
+    "revision": "37b0faafc47a686de1f2",
+    "url": "/appamondo/static/js/1.37b0faaf.chunk.js"
+  },
+  {
+    "revision": "3fb1a0735c8b139113f5",
+    "url": "/appamondo/static/css/main.12e16a84.chunk.css"
+  },
+  {
+    "revision": "6fc9d372a66349eb00eadd17c3bfcede",
+    "url": "/appamondo/index.html"
+  }
+];
